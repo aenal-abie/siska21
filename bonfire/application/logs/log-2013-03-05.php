d4cf0478d7529c14dcc46adc5d910dfda8f7b788
aenal-abie <aenal.abie@gmail.com>
2013-03-05 08:37:51 +0700

    terbaru

---
+++ bonfire/application/logs/log-2013-03-05.php
@@ -0,0 +1,6 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-03-05 01:31:48 --> 404 Page Not Found --> 
+ERROR - 2013-03-05 01:31:49 --> 404 Page Not Found --> 
+ERROR - 2013-03-05 01:31:49 --> 404 Page Not Found --> 
+ERROR - 2013-03-05 01:31:50 --> 404 Page Not Found --> 
